789a9c71c3482b2ab4fbf1a309234ed706b7c172
chrisahelinsky <57368430+chrisahelinsky@users.noreply.github.com>
2019-11-18 14:00:02 -0600

    Update Grid.java

--- Grid.java
+++ Grid.java
@@ -1,27 +1,36 @@
-/* This is a program that attempts to make a  nxn grid
-At present it does not work properly. I am not sure what is wrong */
 import java.util.Scanner;
 import java.util.Arrays;
 
-public class Grid{  
-  /* 0 means the walker has not been here. 1 means it has */
+public class Grid
+{
+	
 	static class gridSpace
 	{
-		int walker1 = 0;
-		int walker2 = 0;
-		int walker3 = 0;
-		int walker4 = 0;
+		int walker1;
+		int walker2;
+		int walker3;
+		int walker4;
+		void init()
+		{
+			walker1 = 0;
+			walker2 = 0;
+			walker3 = 0;
+			walker4 = 0;
+		}
 	}
 	
-	/* creates grid by creating a nxn array of gridSpace objects */
 	static void arrayGrid(int r, int c)
 	{
 		gridSpace[][] arr;
+		int entryStart = 0;
 		arr = new gridSpace[r][c];
 		 for(int b = 0; b < r; b++)
-		    for(int a = 0; a < c;a++)
+		    for(int a = 0; a < c; a++)
+			{
 		      arr[b][a] = new gridSpace();
-		System.out.println(arr[0][0]);
+			  arr[b][a].init();
+			}
+		System.out.println(arr[0][0].walker1);
 	}
 	
 	public static void main(String[] args)
